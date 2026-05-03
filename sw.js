4f91fc648205569889838b5172879b6ea064c15f
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-05-04 05:03:09 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// GANTI VERSI INI KALAU BOS UPDATE KODE HTML BIAR CACHE USER KE-RESET (Misal: v2, v3)
+const CACHE_NAME = 'maktabah-pro-v1'; 
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache Maktabah versi baru siap!');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Menghapus cache jadul:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
